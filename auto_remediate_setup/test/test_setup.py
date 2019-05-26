f306836fcdba16bba2fcc36db6a7f9cb32326eb0
Marat Levit <mlevit@gmail.com>
2019-05-26 16:28:57 +1000

    Added tests for create_stacks()

--- auto_remediate_setup/test/test_setup.py
+++ auto_remediate_setup/test/test_setup.py
@@ -2,9 +2,10 @@ import datetime
 import json
 import logging
 import os
-import py
+import shutil
 
 import moto
+import py
 import pytest
 
 from .. import lambda_handler
@@ -13,12 +14,124 @@ from .. import lambda_handler
 class TestCreateStacks:
     @pytest.fixture
     def setup(self):
-        with moto.mock_cloudformation():
+        with moto.mock_cloudformation(), moto.mock_dynamodb2(), moto.mock_sts():
             setup = lambda_handler.Setup(logging)
             yield setup
 
-    def test_create_stacks(self, setup):
-        setup.create_stacks("config_rules", {})
+    def test_create_stacks_deployment(self, setup):
+        # make mock_rules directory
+        os.mkdir("auto_remediate_setup/data/mock_rules")
+
+        # move mock CloudFormation Stacks
+        shutil.copyfile(
+            "auto_remediate_setup/test/data/mock_rules/cloudtrail-enabled.json",
+            "auto_remediate_setup/data/mock_rules/cloudtrail-enabled.json",
+        )
+
+        # backup settings
+        shutil.move(
+            "auto_remediate_setup/data/auto-remediate-settings.json",
+            "auto_remediate_setup/data/auto-remediate-settings-backup.json",
+        )
+
+        # move mock settings
+        shutil.copyfile(
+            "auto_remediate_setup/test/data/auto-remediate-settings-deploy.json",
+            "auto_remediate_setup/data/auto-remediate-settings.json",
+        )
+
+        # create table
+        setup.client_dynamodb.create_table(
+            TableName="settings-table",
+            KeySchema=[{"AttributeName": "key", "KeyType": "HASH"}],
+            AttributeDefinitions=[{"AttributeName": "key", "AttributeType": "S"}],
+            ProvisionedThroughput={"ReadCapacityUnits": 1, "WriteCapacityUnits": 1},
+        )
+
+        # insert settings to DynamoDB
+        os.environ["SETTINGSTABLE"] = "settings-table"
+        setup.setup_dynamodb()
+
+        # test create_stacks function
+        setup.create_stacks("mock_rules", setup.get_settings())
+
+        # validate Stack created
+        response = setup.client_cloudformation.list_stacks()
+        assert response["StackSummaries"][0]["StackName"] == "cloudtrail-enabled"
+
+        # restore settings
+        shutil.move(
+            "auto_remediate_setup/data/auto-remediate-settings-backup.json",
+            "auto_remediate_setup/data/auto-remediate-settings.json",
+        )
+
+        # delete mock_rules directory
+        shutil.rmtree("auto_remediate_setup/data/mock_rules")
+
+    # def test_create_stacks_removal(self, setup):
+    #     # make mock_rules directory
+    #     os.mkdir("auto_remediate_setup/data/mock_rules")
+
+    #     # move mock CloudFormation Stacks
+    #     shutil.copyfile(
+    #         "auto_remediate_setup/test/data/mock_rules/cloudtrail-enabled.json",
+    #         "auto_remediate_setup/data/mock_rules/cloudtrail-enabled.json",
+    #     )
+
+    #     # backup settings
+    #     shutil.move(
+    #         "auto_remediate_setup/data/auto-remediate-settings.json",
+    #         "auto_remediate_setup/data/auto-remediate-settings-backup.json",
+    #     )
+
+    #     # move mock deploy settings
+    #     shutil.copyfile(
+    #         "auto_remediate_setup/test/data/auto-remediate-settings-deploy.json",
+    #         "auto_remediate_setup/data/auto-remediate-settings.json",
+    #     )
+
+    #     # create table
+    #     setup.client_dynamodb.create_table(
+    #         TableName="settings-table",
+    #         KeySchema=[{"AttributeName": "key", "KeyType": "HASH"}],
+    #         AttributeDefinitions=[{"AttributeName": "key", "AttributeType": "S"}],
+    #         ProvisionedThroughput={"ReadCapacityUnits": 1, "WriteCapacityUnits": 1},
+    #     )
+
+    #     # insert settings to DynamoDB
+    #     os.environ["SETTINGSTABLE"] = "settings-table"
+    #     setup.setup_dynamodb()
+
+    #     # test create_stacks function
+    #     setup.create_stacks("mock_rules", setup.get_settings())
+
+    #     # validate Stack created
+    #     response = setup.client_cloudformation.list_stacks()
+    #     assert response["StackSummaries"][0]["StackName"] == "cloudtrail-enabled"
+
+    #     # move mock remove settings
+    #     shutil.copyfile(
+    #         "auto_remediate_setup/test/data/auto-remediate-settings-remove.json",
+    #         "auto_remediate_setup/data/auto-remediate-settings.json",
+    #     )
+
+    #     # insert settings to DynamoDB
+    #     setup.setup_dynamodb()
+
+    #     # test create_stacks function
+    #     setup.create_stacks("mock_rules", setup.get_settings())
+
+    #     # validate Stack created
+    #     response = setup.client_cloudformation.list_stacks()
+
+    #     # restore settings
+    #     shutil.move(
+    #         "auto_remediate_setup/data/auto-remediate-settings-backup.json",
+    #         "auto_remediate_setup/data/auto-remediate-settings.json",
+    #     )
+
+    #     # delete mock_rules directory
+    #     shutil.rmtree("auto_remediate_setup/data/mock_rules")
 
 
 class TestGetSettings:
@@ -34,11 +147,11 @@ class TestGetSettings:
         Arguments:
             setup {class} -- Instance of Setup class
         """
-        os.environ["SETTINGSTABLE"] = "settings_table"
+        os.environ["SETTINGSTABLE"] = "settings-table"
 
         # create table
         setup.client_dynamodb.create_table(
-            TableName="settings_table",
+            TableName="settings-table",
             KeySchema=[{"AttributeName": "key", "KeyType": "HASH"}],
             AttributeDefinitions=[{"AttributeName": "key", "AttributeType": "S"}],
             ProvisionedThroughput={"ReadCapacityUnits": 1, "WriteCapacityUnits": 1},
@@ -46,7 +159,7 @@ class TestGetSettings:
 
         # populate table
         setup.client_dynamodb.put_item(
-            TableName="settings_table",
+            TableName="settings-table",
             Item={"key": {"S": "version"}, "value": {"N": "1.0"}},
         )
 
@@ -62,17 +175,17 @@ class TestGetSettings:
         Arguments:
             setup {class} -- Instance of Setup class
         """
-        os.environ["SETTINGSTABLE"] = "settings_table"
+        os.environ["SETTINGSTABLE"] = "settings-table"
 
         setup.client_dynamodb.create_table(
-            TableName="settings_table",
+            TableName="settings-table",
             KeySchema=[{"AttributeName": "id", "KeyType": "HASH"}],
             AttributeDefinitions=[{"AttributeName": "id", "AttributeType": "S"}],
             ProvisionedThroughput={"ReadCapacityUnits": 1, "WriteCapacityUnits": 1},
         )
 
         setup.client_dynamodb.put_item(
-            TableName="settings_table", Item={"id": {"S": "123"}}
+            TableName="settings-table", Item={"id": {"S": "123"}}
         )
 
         # test get_settings function
@@ -118,3 +231,27 @@ class TestGetCurrentStacks:
             setup {[type]} -- [description]
         """
         assert setup.get_current_stacks() == []
+
+
+class TestSetupDynamoDb:
+    @pytest.fixture
+    def setup(self):
+        with moto.mock_dynamodb2(), moto.mock_sts():
+            setup = lambda_handler.Setup(logging)
+            yield setup
+
+    def test_setup_dynamodb(self, setup):
+        os.environ["SETTINGSTABLE"] = "settings-table"
+
+        # create table
+        setup.client_dynamodb.create_table(
+            TableName="settings-table",
+            KeySchema=[{"AttributeName": "key", "KeyType": "HASH"}],
+            AttributeDefinitions=[{"AttributeName": "key", "AttributeType": "S"}],
+            ProvisionedThroughput={"ReadCapacityUnits": 1, "WriteCapacityUnits": 1},
+        )
+
+        setup.setup_dynamodb()
+
+        assert len(setup.client_dynamodb.scan(TableName="settings-table")["Items"]) > 0
+
